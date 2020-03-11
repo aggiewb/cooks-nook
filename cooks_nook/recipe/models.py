4716103ec3569e78e964fae356e44058650ea5b4
AggieWB <usmaggie23@gmail.com>
2020-03-11 15:59:31 -0700

    Import user model, and add skeleton for Recipe and Review

--- cooks_nook/recipe/models.py
+++ cooks_nook/recipe/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Recipe(models.Model):
+
+    def __str__(self):
+        return
+
+class Review(models.Model):
+
+    def __str__(self):
+        return
