5f334a179ba6e21bcc192428fffbd0e710bd43d7
AggieWB <usmaggie23@gmail.com>
2020-03-11 16:41:16 -0700

    Update syntax corrections and add datatypes to review
    Add string to return

--- cooks_nook/recipe/models.py
+++ cooks_nook/recipe/models.py
@@ -8,20 +8,40 @@ class Recipe(models.Model):
     CATAGORY_CHOICES=[
         (MEAT, 'Meat'),
         (SEAFOOD, 'Seafood'),
-        (VEGGIE, 'Veggie')
+        (VEGGIE, 'Veggie'),
     ]
-    name=models.CharField(max_length=60)
-    catagory=models.CharField(max_length=7, choices=CATAGORY_CHOICES, default=MEAT)
-    ingredients=ArrayField(models.CharField(max_length=20))
-    prep=models.TextField()
-    servings=models.SmallIntegerField()
-    totalTime=models.SmallIntegerField()
-    user=models.ForeignKey(User, on_delete=models.DO_NOTHING)
+
+    name = models.CharField(max_length=60)
+    catagory = models.CharField(max_length=7, choices=CATAGORY_CHOICES, default=MEAT)
+    ingredients = ArrayField(models.CharField(max_length=20))
+    prep = models.TextField()
+    servings = models.SmallIntegerField()
+    totalTime = models.SmallIntegerField()
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
 
     def __str__(self):
         return self.name
 
+    class Meta:
+        db_table = 'recipe'
+
 class Review(models.Model):
+    GREAT = 'Great'
+    AVERAGE = 'Average'
+    POOR = 'Poor'
+    RATING_CHOICES = [
+        (GREAT, 'Great'),
+        (AVERAGE, 'Average'),
+        (POOR, 'Poor'),
+    ]
+
+    title = models.CharField(max_length=60)
+    rating=models.CharField(max_length=7, choices=RATING_CHOICES, default=GREAT)
+    comments=models.TextField()
+    user=models.ManyToManyField(User)
 
     def __str__(self):
-        return
+        return self.title
+
+    class Meta:
+        db_table = 'review'
