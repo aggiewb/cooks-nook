37e416d14026adda6be7eba8e908627e867bd578
AggieWB <usmaggie23@gmail.com>
2020-03-12 07:42:32 -0700

    Add test_string and test_table for Recipe model

--- cooks_nook/recipe/tests.py
+++ cooks_nook/recipe/tests.py
@@ -1,4 +1,10 @@
 from django.test import TestCase
 from .models import Recipe, Review
 
+class RecipeTest(TestCase):
+    def test_string(self):
+        recipe=Recipe(name='Fried Chicken')
+        self.assertEqual(str(recipe), recipe.name)
 
+    def test_table(self):
+        self.assertEqual(str(Recipe.meta.db_table), 'recipe')
