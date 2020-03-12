192aa320a65e293ce880ac9a3ac6dc239b8b44b0
AggieWB <usmaggie23@gmail.com>
2020-03-12 15:05:13 -0700

    Import user model

--- cooks_nook/recipe/views.py
+++ cooks_nook/recipe/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Recipe, Review
+from .models import Recipe, Review, User
 
 def index(request):
     return render(request, 'recipe/index.html')
