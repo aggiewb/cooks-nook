ddace07f1b40faef6cb12edb85bbbd5a2c7ea6bb
AggieWB <usmaggie23@gmail.com>
2020-03-12 13:08:11 -0700

    Add url paths to view for all recipe catagory types

--- cooks_nook/recipe/urls.py
+++ cooks_nook/recipe/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns=[
     path('', views.index, name='index'),
+    path('veggie/', views.veggieRecipeView, name='veggie'),
+    path('meat/', views.meatRecipeView, name='meat'),
+    path('seafood', views.seafoodRecipeView, name='seafood'),
 ]
