168b801cf84e7f4afb5e5b99b5e7ecd4e0dff022
AggieWB <usmaggie23@gmail.com>
2020-03-12 14:31:55 -0700

    Add url path for recipe details

--- cooks_nook/recipe/urls.py
+++ cooks_nook/recipe/urls.py
@@ -6,4 +6,5 @@ urlpatterns=[
     path('veggie/', views.veggieRecipeView, name='veggie'),
     path('meat/', views.meatRecipeView, name='meat'),
     path('seafood', views.seafoodRecipeView, name='seafood'),
+    path('details/<int:id>', views.recipeDetails, name='recipeDetails'),
 ]
