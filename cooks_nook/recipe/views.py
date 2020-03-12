1392d1644b6195cc90e89b3dc5c0d963a99500c1
AggieWB <usmaggie23@gmail.com>
2020-03-12 13:08:54 -0700

    Add views for all recipe catagory types, and import models

--- cooks_nook/recipe/views.py
+++ cooks_nook/recipe/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
+from .models import Recipe, Review
 
 def index(request):
     return render(request, 'recipe/index.html')
+
+def veggieRecipeView(request):
+    veggieRecipeList=Recipe.objects.filter(catagory='Veggie')
+    return render(request, 'recipe/veggie.html', {'veggieRecipeList': veggieRecipeList})
+
+def meatRecipeView(request):
+    meatRecipeList=Recipe.objects.filter(catagory='Meat')
+    return render(request, 'recipe/meat.html', {'meatRecipeList': meatRecipeList})
+
+def seafoodRecipeView(request):
+    seafoodRecipeList=Recipe.objects.filter(catagory='Seafood')
+    return render(request, 'recipe/seafood.html', {'seafoodRecipeList': seafoodRecipeList})
+
