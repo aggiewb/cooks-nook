969877778b1b0b043cd3a640f55a0943343f1326
AggieWB <usmaggie23@gmail.com>
2020-03-14 08:02:20 -0700

    Update login/logout redirect with name declared in url patterns

--- cooks_nook/cooks_nook/settings.py
+++ cooks_nook/cooks_nook/settings.py
@@ -125,5 +125,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-LOGIN_REDIRECT_URL='login'
-LOGOUT_REDIRECT_URL='logout'
+LOGIN_REDIRECT_URL='login_success'
+LOGOUT_REDIRECT_URL='logout_success'
